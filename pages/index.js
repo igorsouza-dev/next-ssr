fbd316fa86cbb403be682a98cbb027b5cc3b1b2d
Igor Souza <igorsouza.dev@gmail.com>
2019-12-25 20:15:25 -0300

    Understangin HOCs and building an example with Google Analytics

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Link from "next/link";
 import Head from "next/head";
-export default function Index() {
-  return (
-    <div>
-      <Head>
-        <title>Home</title>
-      </Head>
-      <h1>Hello World</h1>
-      <Link href="/users">
-        <a>Users</a>
-      </Link>
-    </div>
-  );
-}
+
+import withAnalytics from "../src/hocs/withAnalytics";
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <h1>Hello World</h1>
+    <Link href="/users">
+      <a>Users</a>
+    </Link>
+  </div>
+);
+
+export default withAnalytics()(Home);
