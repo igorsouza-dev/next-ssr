7f26b58dc1f5aef7020fc8491f9482933a16282d
Igor Souza <igorsouza.dev@gmail.com>
2019-12-25 19:05:27 -0300

    Using links

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import React from "react";
-
+import Link from "next/link";
 export default function Index() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Link href="/users">
+        <a>Users</a>
+      </Link>
+    </div>
+  );
 }
